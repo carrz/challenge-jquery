5deab29a290ce381950c172cfc2cd9dc3c56648e
carrz <carla.zumaran@gmail.com>
2018-01-08 17:34:56 -0300

    Etapa 9: Agrega/quita clase active a las pestañas

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -6,8 +6,12 @@ $('#menu').hide();
 
 $('.js-show-make').click(function(){
 	$('.page').addClass('make');
+	$('.js-show-make').addClass('active');
+	$('.js-show-recipe').removeClass('active');
 });
 
 $('.js-show-recipe').click(function(){
 	$('.page').removeClass('make');
+	$('.js-show-recipe').addClass('active');
+	$('.js-show-make').removeClass('active');
 });
